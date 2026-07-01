7bab96c84686fb1528ee79a0a7c6c255d0fccca0
jaydippatel83 <jaydippatel83.pj@gmail.com>
2026-07-01 07:10:23 +0530

    feat: added user registration and login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,27 +1,48 @@
-const mongoose = require("mongoose");
+const user = require('../model/user');
+const {sign}= require('jsonwebtoken');
+const sendEmail = require('../utils/utils');
+const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema({
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        role: {
-            type: String,
-            enum: ["user", "admin"],
-            default: "user",
-        },
-    },
-    { timestamps: true });
 
-const User = mongoose.model("User", userSchema);
-module.exports = User;
+
+const generateToken = (id) => { 
+    const token = sign({id}, process.env.JWT_SECRET, { expiresIn: "7d" });
+    return token;
+};
+
+
+const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const existingUser = await user.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+        const newUser = new user({ name, email, hashPassword });
+        if (newUser) {
+            const otp = Math.floor(100000 + Math.random() * 900000).toString();
+            const msg = `Welcome to e-commerce ${name}! Thank you for registration with us. You OTP for e-commerce registration is ${otp}`;
+            await sendEmail(email,"Welcome to E-commerce - Your OTP for registation", msg);
+            res.status(201).json({ 
+                _id: newUser._id,
+                name: newUser.name,
+                email: newUser.email,
+                role: newUser.role,
+                token: generateToken(newUser._id),
+
+                message: "User registered successfully. Please check your email for OTP." 
+            });
+        } else {
+            res.status(400).json({ message: "Invalid user data" });
+        } 
+        // await newUser.save();
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+}
+
+const loginUser = async (req, res) => {
+
+}
